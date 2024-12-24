55c709266885d78b050b3d18313d1886133d6a2a
wzsCrossing <3220102674@zju.edu.cn>
2024-12-24 23:33:44 +0800

    数据处理

--- app/charts.tsx
+++ app/charts.tsx
@@ -4,37 +4,84 @@ import ReactECharts from 'echarts-for-react';
 import * as d3 from 'd3';
 
 const Page: React.FC = () => {
-  const [data, setData] = useState<{ trackNumber: number; energy: number }[]>([]);
+  const [data, setData] = useState<{ year: number; trackNumber: number; energy: number; valence: number }[]>([]);
 
   useEffect(() => {
     d3.csv('/top_10000_1950-now.csv').then((csvData) => {
       const parsedData = csvData.map((d) => ({
-        trackNumber: +d['Track Number'], // 假设 CSV 文件中有一列名为 'Track Number'
-        energy: +d['Energy'], // 假设 CSV 文件中有一列名为 'Energy'
+        year: +d['Album Release Date'].slice(0, 4),
+        trackNumber: +d['Track Number'],
+        energy: +d['Energy'],
+        valence: +d['Valence'],
       }));
       setData(parsedData);
     });
   }, []);
 
+  const getAveragesByYear = () => {
+    const yearGroups = data.reduce((acc, { year, energy, valence }) => {
+      if (!acc[year]) {
+        acc[year] = { totalEnergy: 0, totalValence: 0, count: 0 };
+      }
+      acc[year].totalEnergy += energy;
+      acc[year].totalValence += valence;
+      acc[year].count += 1;
+      return acc;
+    }, {} as Record<number, { totalEnergy: number; totalValence: number; count: number }>);
+
+    const averages = Object.entries(yearGroups).map(([year, { totalEnergy, totalValence, count }]) => {
+      return {
+        year: Number(year),
+        avgEnergy: totalEnergy / count,
+        avgValence: totalValence / count,
+      };
+    });
+
+    return averages;
+  };
+
+  const averagesByYear = getAveragesByYear();
+
+  console.log(data);
+
   const options = {
-    grid: { top: 8, right: 8, bottom: 24, left: 36 },
+    grid: { top: 100, right: 8, bottom: 80, left: 36 },
     xAxis: {
       type: 'category',
-      data: data.map(d => d.trackNumber),
+      data: averagesByYear.map(d => d.year),
     },
     yAxis: {
       type: 'value',
     },
+    legend: {
+      show: true,
+      data: ['Energy', 'Valence'],
+    }, 
     series: [
       {
-        data: data.map(d => d.energy),
+        data: averagesByYear.map(d => d.avgEnergy),
         type: 'line',
         smooth: true,
-      },
+      }, {
+        data: averagesByYear.map(d => d.avgValence),
+        type: 'line',
+        smooth: true,
+      }
     ],
     tooltip: {
       trigger: 'axis',
     },
+    dataZoom: [
+      {
+        type: 'inside',
+        start: 0,
+        end: 20
+      },
+      {
+        start: 0,
+        end: 20
+      }
+    ],
   };
 
   return <ReactECharts option={options} />;

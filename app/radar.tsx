2e8b348304d9dcddd3ad29c87fea483f0992c282
wzsCrossing <3220102674@zju.edu.cn>
2024-12-26 01:27:14 +0800

    雷达图修改

--- app/radar.tsx
+++ app/radar.tsx
@@ -3,36 +3,106 @@ import React from 'react';
 import ReactECharts from 'echarts-for-react';
 import { csvReader2 } from './dataLoad';
 
-interface RadarProps {
-  searchQuery: string;
-}
+const genreFromString = (genre: string): number => {
+  switch (genre) {
+    case 'pop': return 1;
+    case 'country': return 2;
+    case 'blues': return 3;
+    case 'rock': return 4;
+    case 'jazz': return 5;
+    default: return 0;
+  }
+};
 
-const Radar: React.FC<RadarProps> = ({ searchQuery }) => {
+const Radar: React.FC = () => {
   const data = csvReader2();
 
-	let dataMap = data.map((d) => [
-		d.year,
-		d.author,
-		d.trackName,
-		d.genre,
-		d.lyrics,
-		d.topic,
-		d.romantic,
-		d.violence,
-		d.world_life,
-		d.shake_the_audience,
-		d.family,
-		d.obscene,
-		d.music,
-		d.sadness,
-		d.feelings,
-	]);
+  const getAveragesByGenre = () => {
+    const GenreGroups = data.reduce((acc, {
+      genre,
+      romantic,
+      violence,
+      world_life,
+      shake_the_audience,
+      family,
+      obscene,
+      music,
+      sadness,
+      feelings,
+    }) => {
+      const index = genreFromString(genre);
+      if (index === 0) {
+        return acc;
+      }
+      if (!acc[index]) {
+        acc[index] = { 
+          totalRomantic: 0,
+          totalViolence: 0,
+          totalWorld_life: 0,
+          totalShake_the_audience: 0,
+          totalFamily: 0,
+          totalObscene: 0,
+          totalMusic: 0,
+          totalSadness: 0,
+          totalFeelings: 0,
+          count: 0
+        };
+      }
+      acc[index].totalRomantic += romantic;
+      acc[index].totalViolence += violence;
+      acc[index].totalWorld_life += world_life;
+      acc[index].totalShake_the_audience += shake_the_audience;
+      acc[index].totalFamily += family;
+      acc[index].totalObscene += obscene;
+      acc[index].totalMusic += music;
+      acc[index].totalSadness += sadness;
+      acc[index].totalFeelings += feelings;
+      acc[index].count += 1;
+      return acc;
+    }, {} as Record<number, { 
+      totalRomantic: number,
+      totalViolence: number,
+      totalWorld_life: number,
+      totalShake_the_audience: number,
+      totalFamily: number,
+      totalObscene: number,
+      totalMusic: number,
+      totalSadness: number,
+      totalFeelings: number,
+      count: number
+    }>);
 
-	if (searchQuery) {
-		dataMap = dataMap.filter((d) => d[1].toString().toLowerCase().includes(searchQuery.toLowerCase()));
-	} else {
-		dataMap = [];
-	}
+    const averages = Object.entries(GenreGroups).map(([genre, {
+      totalRomantic,
+      totalViolence,
+      totalWorld_life,
+      totalShake_the_audience,
+      totalFamily,
+      totalObscene,
+      totalMusic,
+      totalSadness,
+      totalFeelings,
+      count
+    }]) => {
+      return {
+        avgRomantic: totalRomantic / count,
+        avgViolence: totalViolence / count,
+        avgWorld_life: totalWorld_life / count,
+        avgShake_the_audience: totalShake_the_audience / count,
+        avgFamily: totalFamily / count,
+        avgObscene: totalObscene / count,
+        avgMusic: totalMusic / count,
+        avgSadness: totalSadness / count,
+        avgFeelings: totalFeelings / count,
+      };
+    });
+
+    return averages;
+  };
+
+  const averagesByGenre = getAveragesByGenre();
+
+  console.log(averagesByGenre);
 
   const lineStyle = {
     width: 1,
@@ -101,7 +171,7 @@ const Radar: React.FC<RadarProps> = ({ searchQuery }) => {
     series: {
       type: 'radar',
       lineStyle: lineStyle,
-      data: dataMap.map((d) => (d.slice(6))),
+      data: averagesByGenre,
       symbol: 'none',
       itemStyle: {
         color: '#F9713C'
